f1d350154858499d0d4eb9650e32b7a55ec516b0
Filippo De Togni <detognifilippo@gmail.com>
2018-06-06 14:05:01 +0200

    Aggiunta una seconda schermata

--- Dizionario Emoji/ViewController.swift
+++ Dizionario Emoji/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var mainTableView: UITableView!
+    
+    var emojis = ["😀","🙃","🧐","🤔","🤡","🤯"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mainTableView.dataSource = self
+        mainTableView.delegate = self
+        
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count // Ci sono tante righe del TableView quanti sono gli elementi dell'array. Geniale.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
